510b7b3f11ea84c3a0f378496506655d6066a253
Mahad Khan <mahad.khan@optimusfox.com>
2024-08-12 18:20:23 +0500

    second commit

--- bot/pythonProject/bot/chatbot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import QuPortChatbotView, PadelChatbotView, SoccerChatbotView, BakermanChatbotView
-
-urlpatterns = [
-    path('padel/', PadelChatbotView.as_view(), name='padel'),
-    path('quport/', QuPortChatbotView.as_view(), name='quport'),
-    path('soccer/', SoccerChatbotView.as_view(), name='soccer'),
-    path('bakerman/', BakermanChatbotView.as_view(), name='bakerman')
-]
